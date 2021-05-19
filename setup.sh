86c286830f4530e96137d9f0a8d9a63f310eddcb
DZX <DZX@ravpn-266-2-student-ten-8-88-15.uniaccess.unimelb.edu.au>
2021-05-19 11:45:12 +0800

    docker, couchdb

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-. ./unimelb-comp90024-2021-grp-33-openrc.sh; ansible-playbook --ask-become-pass setup.yaml -i inventory/inventory.ini
+. ./unimelb-comp90024-2021-grp-33-openrc.sh; ansible-playbook setup.yaml -i inventory/inventory.ini
