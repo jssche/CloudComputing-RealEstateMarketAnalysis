45bc2b5528868efc3a2116d5afe32904156c6c19
Si Cheng <sccheng2@student.unimelb.edu.au>
2021-05-20 18:18:41 +1000

    added test frontend connect db

--- twitterHarvester.py
+++ twitterHarvester.py
@@ -1,20 +1,40 @@
 import os
+import re
+import time
 import json
 import requests
 import tweepy
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-consumer_key = 'W3nWSuPyudnw8142u58LNXiTc'
-consumer_secret = 'cNTNL1tBB9lQKNaIr11u1CLv0IMBRzc81JS7QqRLCNXy6b334p'
-access_token = '3149835139-Ey1XqWLn6Mk1MFKcHbTtaDJ9NZUETZJYgISfLjW'
-token_secret = 'Y0ZffGkSBbaYYSkvitsEunU9gyj2EAERWMxUhAaiPe30k'
+class TwitterHarvester():
+    def __init__(self):
+        _consumer_key = 'W3nWSuPyudnw8142u58LNXiTc'
+        _consumer_secret = 'cNTNL1tBB9lQKNaIr11u1CLv0IMBRzc81JS7QqRLCNXy6b334p'
+        _access_token = '3149835139-Ey1XqWLn6Mk1MFKcHbTtaDJ9NZUETZJYgISfLjW'
+        _token_secret = 'Y0ZffGkSBbaYYSkvitsEunU9gyj2EAERWMxUhAaiPe30k'
+        
 
-callback_url = 'oob' # our url
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, token_secret)
-api = tweepy.API(auth)
 
-cursor = tweepy.Cursor(api.user_timeline,id='RayWhiteGroup').items(30)
-for i in cursor:
-    print(i.created_at)
+
+
+def create_db(name):
+    headers = {'content-type': 'application/json'}
+    url = 'http://admin:admin@172.26.134.87:5984/' + name
+    r = requests.put(url)
+    print(r.text)
+
+
+
+def main():
+    # create_db('twitter')
+    
+
+    analyzer = SentimentIntensityAnalyzer()
+
+
+if __name__ == "__main__":
+    main()
 
