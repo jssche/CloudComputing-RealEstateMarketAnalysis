62c1a2d661524e3ab3e574e4cc3b542059269f01
DZX <DZX@ravpn-266-2-student-ten-8-93-73.uniaccess.unimelb.edu.au>
2021-05-26 06:37:19 +0800

    Removed

--- instances.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-. ./unimelb-comp90024-2021-grp-33-openrc.sh; ansible-playbook instances.yaml
--- setup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-. ./unimelb-comp90024-2021-grp-33-openrc.sh; ansible-playbook setup.yaml -i inventory/inventory.ini
